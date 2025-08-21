fbd69feea7d08abb669cb94d8d707a50d13a089f
Advansed <atlasov.n.r@gmail.com>
2025-08-21 11:56:15 +0900

    12

--- src/components/Profile/components/ProfileStats.tsx
+++ src/components/Profile/components/ProfileStats.tsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import { UserRatings } from '../types'
 import { STAT_LABELS } from '../constants'
-
+import './ProfileStats.css'
 interface Props {
   ratings: UserRatings
   userType: number
 }
 
-const StatBlock: React.FC<{value: number, label: string}> = ({ value, label }) => (
-  <div className="bg-3 pb-05 pt-05 pl-05 pr-05">
-    <div className="a-center"><b>{value}</b></div>
-    <div className="fs-07 a-center mt-05">{label}</div>
+const StatBlock: React.FC<{value: number, label: string, index: number}> = ({ value, label, index }) => (
+  <div className={`cr-status-${index + 1} profile-stat-enhanced`}>
+    <div className="a-center fs-15"><b>{value}</b></div>
+    <div className="fs-09 a-center mt-05">{label}</div>
   </div>
 )
 
 export const ProfileStats: React.FC<Props> = React.memo(({ ratings, userType }) => (
   <div className="flex mt-1 fl-space ml-1 mr-1">
-    <StatBlock value = { ratings.orders } label = { STAT_LABELS.ORDERS } />
-    <StatBlock value = { ratings.rate } label = { STAT_LABELS.RATING } />
     <StatBlock 
-      value={ userType == 2 ? ratings.invoices : ratings.payd } 
-      label={ userType === 2 ? STAT_LABELS.INVOICES : STAT_LABELS.PAID } 
+        value={ratings.orders} 
+        label={STAT_LABELS.ORDERS} 
+        index={0} 
+    />
+    <StatBlock 
+        value={ratings.rate} 
+        label={STAT_LABELS.RATING} 
+        index={1} 
+    />
+    <StatBlock 
+      value={userType == 2 ? ratings.invoices : ratings.payd} 
+      label={userType === 2 ? STAT_LABELS.INVOICES : STAT_LABELS.PAID} 
+      index={2}
     />
   </div>
 ))
